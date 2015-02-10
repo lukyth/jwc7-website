fc599d5fe918a16c7b8e0c5a094d54fd194f0b44
Kosate Limpongsa <kosatelim@gmail.com>
2015-02-11 02:43:59 +0700

    Main page wireframe.

---
+++ applicationJWC7a/views/main.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
+	<title>JWC#7</title>
+
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="<?= base_url()."assets/" ?>css/default_global.css"/>
+	<link rel="stylesheet" type="text/css" href="<?= base_url()."assets/" ?>css/bootstrap.min.css"/>
+	<link rel="stylesheet" type="text/css" href="<?= base_url()."assets/" ?>css/main.css" />
+
+</head>
+	
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-content">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				</button>
+			</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="text-center">
+				<div class="collapse navbar-collapse" id="navbar-content">
+					<ul class="nav navbar-nav">
+						<li><a href="#">รายละเอียด</a></li>
+						<li><a href="#">สาขา</a></li>
+						<li><a href="#">ผู้สนับสนุน</a></li>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">ถาม - ตอบ</a></li>
+						<li><a href="#">Timeline</a></li>
+						<li><a href="#">ภาพจากปีเก่า</a></li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div>
+		</div><!-- /.container-fluid -->
+	</nav>
+
+	<div id="header-page">
+		<div class="cover-img" src="<?= base_url() ?>assets/img/main/city.png" alt="">
+			<div class="row">
+				<div class="col-xs-12">
+					<img class="img-center" style="margin-top:150px;" src="<?= base_url() ?>assets/img/main/logo.png" />
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fuild" style="overflow:hidden;">
+		<div id="detail-page" class="page">
+			<div class="row">
+				<div class="col-xs-12">
+					<h1 class="title text-center">รายละเอียด</h1>
+					<p class="text-center">
+						XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br>
+						XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br>
+						XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br>
+						XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br>
+						XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br>
+						XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br>
+						XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br>
+						XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br>
+						XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br>
+						<br>
+						XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br>
+						XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br>
+						XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br>
+						XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br>
+						XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br>
+						XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br>
+						XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br>
+						XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br>
+						XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX<br>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div id="category-page" class="page">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1">
+					<h1 class="title text-center">สาขา</h1>
+					<div class="row">
+						<div class="col-md-4 text-center">
+							<div class="img-center" style="width:400px; height:500px; background:#111; border-radius:10px;"></div>
+						</div>
+						<div class="col-md-4 text-center">
+							<div class="img-center" style="width:400px; height:500px; background:#111; border-radius:10px;"></div>
+						</div>
+						<div class="col-md-4 text-center">
+							<div class="img-center" style="width:400px; height:500px; background:#111; border-radius:10px;"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="category-page" class="page">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1">
+					<h1 class="title text-center">ผู้สนับสนุน</h1>
+					<div class="row">
+						<div class="col-md-3 text-center">
+						</div>
+						<div class="col-md-3 text-center">
+							<div class="img-center" style="width:240px; height:180px; background:#111; border-radius:10px;"></div>
+						</div>
+						<div class="col-md-3 text-center">
+							<div class="img-center" style="width:240px; height:180px; background:#111; border-radius:10px;"></div>
+						</div>
+						<div class="col-md-3 text-center">
+						</div>
+					</div>
+					<div class="row" style="margin-top:50px;">
+						<div class="col-md-2"></div>
+						<div class="col-md-2 text-center">
+							<div class="img-center" style="width:200px; height:150px; background:#111; border-radius:10px;"></div>
+						</div>
+						<div class="col-md-2 text-center">
+							<div class="img-center" style="width:200px; height:150px; background:#111; border-radius:10px;"></div>
+						</div>
+						<div class="col-md-2 text-center">
+							<div class="img-center" style="width:200px; height:150px; background:#111; border-radius:10px;"></div>
+						</div>
+						<div class="col-md-2 text-center">
+							<div class="img-center" style="width:200px; height:150px; background:#111; border-radius:10px;"></div>
+						</div>
+						<div class="col-md-2"></div>
+					</div>
+					<div class="row" style="margin-top:50px;">
+						<div class="col-md-4"></div>
+						<div class="col-md-1 text-center">
+							<div class="img-center" style="width:80px; height:60px; background:#111; border-radius:10px;"></div>
+						</div>
+						<div class="col-md-1 text-center">
+							<div class="img-center" style="width:80px; height:60px; background:#111; border-radius:10px;"></div>
+						</div>
+						<div class="col-md-1 text-center">
+							<div class="img-center" style="width:80px; height:60px; background:#111; border-radius:10px;"></div>
+						</div>
+						<div class="col-md-1 text-center">
+							<div class="img-center" style="width:80px; height:60px; background:#111; border-radius:10px;"></div>
+						</div>
+						<div class="col-md-4"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="timeline-page" class="page">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1">
+					<h1 class="title text-center">Time line</h1>
+					<img class="img-center" style="margin-top:40px;" src="<?= base_url() ?>assets/img/main/timeline.jpg" alt="" />
+				</div>
+			</div>
+		</div>
+		<div id="qa-page" class="page">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<h1 class="title text-center">ถาม - ตอบ</h1>
+					<div class="row">
+						<div class="col-md-7">
+							<div class="img-center" style="width:100%; height:100px; background:#111; border-radius:10px;"></div>
+						</div>
+					</div>
+					<div class="row" style="margin-top:30px;">
+						<div class="col-md-offset-5 col-md-7">
+							<div class="img-center" style="width:100%; height:100px; background:#111; border-radius:10px;"></div>
+						</div>
+					</div>
+					<div class="row" style="margin-top:30px;">
+						<div class="col-md-7">
+							<div class="img-center" style="width:100%; height:100px; background:#111; border-radius:10px;"></div>
+						</div>
+					</div>
+					<div class="row" style="margin-top:30px;">
+						<div class="col-md-offset-5 col-md-7">
+							<div class="img-center" style="width:100%; height:100px; background:#111; border-radius:10px;"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="picture-page" class="page">
+			<div class="row">
+				<div class="col-md-12">
+					<h1 class="title text-center">ภาพจากปีเก่า</h1>
+					<div class="row">
+						<div class="col-xs-2 text-center" style="font-size:70px; font-weight:bold; height:630px;">
+							<div style="display:table; width:100%; height:100%;">
+								<div style="display:table-cell; vertical-align:middle; float:none;">
+									&lt;
+								</div>
+							</div>
+						</div>
+						<div class="col-xs-8">
+							<div class="row">
+								<div class="col-md-2">
+									<div class="img-center" style="width:200px; height:200px; background:#222; margin:5px;"></div>
+								</div>
+								<div class="col-md-2">
+									<div class="img-center" style="width:200px; height:200px; background:#222; margin:5px;"></div>
+								</div>
+								<div class="col-md-2">
+									<div class="img-center" style="width:200px; height:200px; background:#222; margin:5px;"></div>
+								</div>
+								<div class="col-md-2">
+									<div class="img-center" style="width:200px; height:200px; background:#222; margin:5px;"></div>
+								</div>
+								<div class="col-md-2">
+									<div class="img-center" style="width:200px; height:200px; background:#222; margin:5px;"></div>
+								</div>
+								<div class="col-md-2">
+									<div class="img-center" style="width:200px; height:200px; background:#222; margin:5px;"></div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-2">
+									<div class="img-center" style="width:200px; height:200px; background:#222; margin:5px;"></div>
+								</div>
+								<div class="col-md-2">
+									<div class="img-center" style="width:200px; height:200px; background:#222; margin:5px;"></div>
+								</div>
+								<div class="col-md-2">
+									<div class="img-center" style="width:200px; height:200px; background:#222; margin:5px;"></div>
+								</div>
+								<div class="col-md-2">
+									<div class="img-center" style="width:200px; height:200px; background:#222; margin:5px;"></div>
+								</div>
+								<div class="col-md-2">
+									<div class="img-center" style="width:200px; height:200px; background:#222; margin:5px;"></div>
+								</div>
+								<div class="col-md-2">
+									<div class="img-center" style="width:200px; height:200px; background:#222; margin:5px;"></div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-2">
+									<div class="img-center" style="width:200px; height:200px; background:#222; margin:5px;"></div>
+								</div>
+								<div class="col-md-2">
+									<div class="img-center" style="width:200px; height:200px; background:#222; margin:5px;"></div>
+								</div>
+								<div class="col-md-2">
+									<div class="img-center" style="width:200px; height:200px; background:#222; margin:5px;"></div>
+								</div>
+								<div class="col-md-2">
+									<div class="img-center" style="width:200px; height:200px; background:#222; margin:5px;"></div>
+								</div>
+								<div class="col-md-2">
+									<div class="img-center" style="width:200px; height:200px; background:#222; margin:5px;"></div>
+								</div>
+								<div class="col-md-2">
+									<div class="img-center" style="width:200px; height:200px; background:#222; margin:5px;"></div>
+								</div>
+							</div>
+						</div>
+						<div class="col-xs-2 text-center" style="font-size:70px; font-weight:bold; height:630px;">
+							<div style="display:table; width:100%; height:100%;">
+								<div style="display:table-cell; vertical-align:middle; float:none;">
+									&gt;
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
