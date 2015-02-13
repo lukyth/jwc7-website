431cdca92c1f4802daaca263120129c15acb86c8
Pichet <pichet.nk@gmail.com>
2015-02-13 21:21:07 +0700

    update DB

---
+++ DB/table_homework.sql
@@ -0,0 +1,27 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.6.16 - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.1.0.4867
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table ywc_jwc7.homework
+CREATE TABLE IF NOT EXISTS `homework` (
+  `facebookID` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `q1` longtext CHARACTER SET utf8,
+  `q2` longtext CHARACTER SET utf8,
+  `q3` longtext CHARACTER SET utf8,
+  `q4` longtext CHARACTER SET utf8,
+  `q5` longtext CHARACTER SET utf8,
+  PRIMARY KEY (`facebookID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
