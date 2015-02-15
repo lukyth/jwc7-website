9e535b8366244e9b4ce657cb9cdb7514fd7a7e44
Pichet <pichet.nk@gmail.com>
2015-02-15 21:51:22 +0700

    add google analytics

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php
 
 /*
@@ -203,3 +204,15 @@ require_once BASEPATH.'core/CodeIgniter.php';
 
 /* End of file index.php */
 /* Location: ./index.php */
+
+?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-59770266-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
