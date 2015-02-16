c8d7d8b2ee043518c8764cbdf64a90ceb3ccd02c
Lukyth <k.sujautra@gmail.com>
2015-02-17 01:02:44 +0700

    fix email

---
+++ applicationJWC7a/config/email.php
@@ -0,0 +1,14 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'localhost';
+$config['smtp_port'] = '587';
+$config['smtp_timeout'] = '100';
+$config['smtp_user'] = 'jwc7@ywc.in.th';
+$config['smtp_pass'] = 'Wineuusee()07';
+$config['smtp_crypto'] = 'tls';
+$config['charset'] = 'utf-8';
+$config['newline'] = "\r\n";
+$config['mailtype'] = 'html'; // or html
+$config['validation'] = TRUE; // bool whether to validate email or not
