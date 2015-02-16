134361e685fe33b77fa799d22248895c68fb2f26
Kosate Limpongsa <kosatelim@gmail.com>
2015-02-16 17:36:00 +0700

    sponsor size

--- applicationJWC7a/views/main.php
+++ applicationJWC7a/views/main.php
@@ -182,7 +182,7 @@
 			</div>
 			<div class="container">
 				<div class="row">
-					<div class="col-md-12">
+					<div class="col-xs-8 col-xs-offset-2">
 						<div class="row">
 							<div class="col-md-4 col-xs-6 text-center">
 								<img class="img-center img-responsive" src="<?= base_url() ?>assets/img/main/sponsor_large.png" />
