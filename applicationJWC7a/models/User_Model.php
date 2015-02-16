48c4341aa4029a41cdae890e722da97e5dc9f71d
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2015-02-16 14:06:00 +0700

    Merged with main branch

--- applicationJWC7a/models/User_Model.php
+++ applicationJWC7a/models/User_Model.php
@@ -23,5 +23,20 @@
         return $query->num_rows();       
     }
 
+    function login($username, $password) {
+       $query = $this->db->get_where('users', array('username' => $username,'password'=>sha1($password)));
+
+       if($query -> num_rows() == 1) {
+
+           $result=$query->result();
+           $this->id=$result[0]->id;
+           $this->username=$result[0]->username;
+           $this->permission=$result[0]->permission;
+           return true;
+       } else {
+           return false;
+       }
+    }
+
 
 }
