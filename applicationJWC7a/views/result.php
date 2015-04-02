7616c70b69fd667249b2a9c86a999dd5a7002903
Lukyth <k.sujautra@gmail.com>
2015-04-02 12:11:59 +0700

    init result page

---
+++ applicationJWC7a/views/result.php
@@ -0,0 +1,203 @@
+<!doctype html>
+<html>
+<head>
+  <title>Junior Webmaster Camp #7 | สมัครสมาชิก</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" type="text/css" href="<?= base_url()."assets/" ?>css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url()."assets/" ?>css/result.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url()."assets/" ?>fonts/csprajad.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url()."assets/" ?>fonts/wdb_bangna.css">
+  <script type="text/javascript" src="<?= base_url()."assets/" ?>js/jquery-1.11.1.min.js"></script>
+  <link rel="icon" type="image/png" href="<?= base_url()."assets/" ?>img/favicon.png">
+  <script>
+    $(document).ready(function(){
+      $('a').click(function(){
+        console.log('Hello');
+          $('html, body').animate({
+              scrollTop: $( $.attr(this, 'href') ).offset().top
+          }, 500);
+          return false;
+      });
+    });
+  </script>
+</head>
+<body>
+  <div class="row" id="head">
+    <div class="container-fluid txt-center">
+      <div class="col-sm-12">
+        <a href="http://jwc.in.th"><img src="http://jwc.in.th/jwc7/assets/img/logo.svg" alt="logo" id="logo"></a>
+      </div>
+    </div>
+  </div>
+  <div class="row" id="body">
+    <div class="container-fluid txt-center">
+      <div class="col-xs-12">
+      <div class="row design" id="form-head">
+        <div class="col-sm-10 col-sm-offset-1" style="padding-bottom:20px;">
+          <h1>ประกาศผลการสมัครเข้าค่าย JWC7</h1>
+        </div>
+      </div>
+      <div class="row" id="regisform">
+
+<!--////////////////////////////////////////////////////////////////////////////////////////////////-->
+
+          <div class="col-sm-10 col-sm-offset-1">
+            <!-- <h2>ขอแสดงความยินดีกับน้องๆ ที่ได้เข้าค่าย JWC7 ทุกคน</h2>
+            <hr> -->
+            <br>
+            <p><b>*****รอ text จริง*****</b></p>
+            <p class="txt-left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ขอแสดงความยินดีกับน้องๆ ที่ผ่านด่านหฤโหด จากผู้สมัคร 1,130 คน มาเป็นตัวจริง 42 คนที่ได้ไป Junior Webmaster Camp 7 ส่วนคนที่ไม่ผ่าน สะสมประสบการณ์มาประชันกันใหม่ปีหน้า อย่าเพิ่งถอดใจนะ โอกาสเป็นของคนที่พร้อมเสมอ สู้ๆ ค่ะ</p>
+            <p class="txt-left">สำหรับน้องๆ ที่ติดตัวจริง ให้ดำเนินการ 3 อย่างนี้ ภายในวันที่ <b>5 ธันวาคม 2557</b></p>
+            <ol class="txt-left">
+              <li><b>โอนเงินค่ามัดจำ 300 บาท</b> เพื่อยืนยันว่าสามารถไปค่ายได้จริง ซึ่งจะคืนในวันจบค่าย</li>
+              <li><b>ส่งสำเนาบัตรนักเรียน</b> (หรือทรานสคริปต์)</li>
+              <li><b>หลักฐานการโอนเงิน</b> เพื่อยืนยันตัวตน</li>
+            </ol>
+            <p class="txt-left"></p>
+            <p class="txt-left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;หากตัวจริงไม่ยืนยันสิทธิ์ ภายในเวลาที่กำหนด สิทธิ์จะตกเป็นของตัวสำรองตามลำดับรายชื่อ ไม่ว่ากรณีใดๆ ทั้งสิ้น โดยทางค่ายจะติดต่อตัวสำรองเมื่อมีการสละสิทธิ์เกิดขึ้น</p>
+            </p>
+            <div class="txt-center">
+              <div class="btn btn-danger btn-lg nextbtn" href="">ยืนยันสิทธิ์ &raquo;</div>
+              <p class="deadline"><b>ภายใน 5 มีนาคมนี้ เท่านั้น!</b></p>
+              <p><b>[f share] <[127]</b></p>
+            </div>
+            <br>
+            <h2>รายชื่อผู้ผ่านการคัดเลือกเข้าค่าย JWC7</h2>
+            <hr>
+            <div class="row">
+              <a class="btn btn-design btn-lg" href="#design" style="padding-bottom:0; max-width:200px;">
+                ดูรายชื่อสาขา
+                <br><b>Web Design</b> &raquo;
+                <img class="img-responsive" src="<?= base_url()."assets/" ?>img/mascot_ds.svg">
+              </a>
+              <a class="btn btn-content btn-lg" href="#content" style="padding-bottom:0; max-width:200px;">
+                ดูรายชื่อสาขา
+                <br><b>Web Content</b> &raquo;
+                <img class="img-responsive" src="<?= base_url()."assets/" ?>img/mascot_ct.svg">
+              </a>
+              <a class="btn btn-marketing btn-lg" href="#marketing" style="padding-bottom:0; max-width:200px;">
+                ดูรายชื่อสาขา
+                <br><b>Web Marketing</b> &raquo;
+                <img class="img-responsive" src="<?= base_url()."assets/" ?>img/mascot_mkt.svg">
+              </a>
+            </div>
+            <br>
+          </div>
+          <div class="row">
+            <div class="col-sm-10 col-sm-offset-1" id="design">
+              <table class="table list">
+                <thead>
+                  <tr><th class="txt-center design" colspan="2"><h2 class="listhead">สาขา Web Design</h2></th></tr>
+                </thead>
+                <tbody>
+                  <tr><td>1. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>2. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>3. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>4. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>5. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>6. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>7. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>8. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>9. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>10. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>11. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>12. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>13. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>14. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                </tbody>
+                <thead>
+                  <tr><th class="txt-center design" colspan="2">สำรอง Web Design</th></tr>
+                </thead>
+                <tbody>
+                  <tr><td>1. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>2. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>3. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>4. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <br>
+          <div class="row">
+            <div class="col-sm-10 col-sm-offset-1" id="content">
+              <table class="table list">
+                <thead>
+                  <tr><th class="txt-center content" colspan="2"><h2 class="listhead">สาขา Web Content</h2></th></tr>
+                </thead>
+                <tbody>
+                  <tr><td>1. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>2. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>3. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>4. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>5. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>6. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>7. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>8. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>9. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>10. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>11. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>12. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>13. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>14. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                </tbody>
+                <thead>
+                  <tr><th class="txt-center content" colspan="2">สำรอง Web Content</th></tr>
+                </thead>
+                <tbody>
+                  <tr><td>1. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>2. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>3. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>4. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <br>
+          <div class="row">
+            <div class="col-sm-10 col-sm-offset-1" id="marketing">
+              <table class="table list">
+                <thead>
+                  <tr><th class="txt-center marketing" colspan="2"><h2 class="listhead">สาขา Web Marketing</h2></th></tr>
+                </thead>
+                <tbody>
+                  <tr><td>1. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>2. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>3. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>4. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>5. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>6. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>7. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>8. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>9. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>10. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>11. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>12. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>13. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>14. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                </tbody>
+                <thead>
+                  <tr><th class="txt-center marketing" colspan="2">สำรอง Web Marketing</th></tr>
+                </thead>
+                <tbody>
+                  <tr><td>1. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>2. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                  <tr><td>3. นายสมมติ ไม่ทราบนามสกุล</td>
+                    <td>4. นายสมมติ ไม่ทราบนามสกุล</td></tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <br>
+          <div class="txt-center">
+            <div class="btn btn-danger btn-lg nextbtn" href="">ยืนยันสิทธิ์ &raquo;</div>
+            <p class="deadline"><b>ภายใน 5 มีนาคมนี้ เท่านั้น!</b></p>
+            <p><b>[f share] <[127]</b></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
